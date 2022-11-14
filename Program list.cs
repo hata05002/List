20f171f5ac0567ec3274a6f34f6727e21bcb6ea8
hata05002 <112382754+hata05002@users.noreply.github.com>
2022-11-14 09:01:22 +0100

    Add files via upload

---
+++ Program list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class program{
+    public static void Main(){
+        List <double> tal= new List<double>();
+        tal.Add(1.3);
+        tal.Add(2.7);
+        tal.Add(3.8);
+        tal.Add(4.8);
+        tal.Add(6.8);
+        // add 5 element, remove index 3
+        tal.RemoveAt(3);
+
+        // write out each element 
+        foreach( double e in tal){
+            Console.WriteLine(e);
+        }
+        
+    
+        Medel(tal,(tal.Count));
+
+    //Medel method    
+    static void Medel(List<double>l, int element){
+
+     double sum= l.Sum();
+     Console.WriteLine(sum/element);
+
+    }
+      
+   }
+        
+}
